1dc42a46edb76237a6febf29acc422adff82017a
warrenL <zpli1984@hotmail.com>
2018-04-12 22:48:57 +0800

    add repare api service

---
+++ src/provider/http/RepareHttpProvider.ts
@@ -0,0 +1,153 @@
+import { Injectable, ApplicationRef } from '@angular/core';
+
+import { GlobalProvider } from '../GlobalProvider';
+
+import { HttpProvider } from "./base/HttpProvider";
+import { ValueTransformer } from '@angular/compiler/src/util';
+import { HttpException } from "../../exception/HttpException";
+
+/**
+ * The provider class repare majo machine to provide http api for user.
+ *
+ */
+@Injectable()
+export class RepareHttpProvider {
+
+    /*
+    all the response should be follow below format:
+    {
+      "data":{
+
+      }
+      "reMsg":"操作成功",
+      "reCode":200
+    }
+    */
+
+   listData=[];
+    constructor(private httpProvider: HttpProvider, private globalProvider: GlobalProvider, private ref: ApplicationRef) { }
+
+    private handleError(error: Response) {
+      console.error("http error - " + error);
+      throw new HttpException(error.status, "Http Request Error");
+    }
+
+    // 5.  维修麻将机未接订单
+    //     地址:	api/repare/orders
+    //     参数:	addr1, addr2, addr3, addr4, token, orderType(0所有订单, 1维修订单, 2购买新机器订单, 3购买旧机器订单, 4租麻将机订单, 5安装麻将机或配件订单)
+    //     返回:	orderNo, orderType(1维修订单, 2购买新机器订单, 3购买旧机器订单, 4租麻将机订单, 5安装麻将机或配件订单), 
+    //       publisher, remark(描述), addr1, addr2, addr3, addr4, addr5, phone, publishDate, topFlag(1置顶,0不置顶)  
+    repareOraders(addr1: string, addr2: string, addr3: string, addr4: string): Promise<any> {
+      let url = "api/repare/orders";
+      let heads = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'};
+      let params = 'addr1=' + addr1 + '&addr2=' + addr2 + '&addr3=' + addr3 + '&addr4=' + addr4;
+
+      return this.httpProvider.httpPostWithAuth(url, heads, params).then((value) => {
+        console.log(value);
+        if (value.reCode == 200 && value.reMsg == "操作成功") {
+          return value.data;
+        } else {
+          return value.reMsg;
+        }
+      }).catch((error) => {
+        console.log(error);
+        this.handleError(error);
+      });
+    }
+    
+    // 6.  维修麻将机(申请通过, 申请中)
+    //     地址:	api/repare/myOrders
+    //     参数:	token, type(1申请中, 2申请通过, 3我发布的订单)
+    //     返回:	orderNo, publisher, remark(描述), addr1, addr2, addr3, addr4, addr5, phone, publishDate, 
+    //       topFlag(1置顶,0不置顶), status(1申请中, 2申请通过, 3已完成), reqDate(申请日期), approveDate(订单审批通过日期)
+    //       reqCount(申请数量)
+    repareMyOrders(type: string): Promise<any> {
+      let url = "api/repare/myOrders";
+      let heads = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'};
+      let params = 'type' + type;
+
+      return this.httpProvider.httpPostWithAuth(url, heads, params).then((value) => {
+        console.log("Response " + value);
+        if (value.reCode == 200 && value.reMsg == "操作成功") {
+          return value.data;
+        } else {
+          return value.reMsg;
+        }
+      }).catch((error) => {
+        console.log(error);
+        this.handleError(error);
+      });
+    }
+    
+    // 7.  维修麻将机取消申请/举报/完成/置顶/上架/下架/删除
+    //     地址:	api/repare/orderUpdate
+    //     参数:	token, orderNo, type(1取消 2举报, 3完成, 4置顶, 5上架, 6下架, 7删除)
+    //     返回:
+    repareOrderUpdate(orderNo: string, type: string): Promise<any> {
+      let url = "api/repare/orderUpdate";
+      let heads = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'};
+      let params = 'orderNo=' + orderNo + '&type=' + type;
+
+      return this.httpProvider.httpPostWithAuth(url, heads, params).then((value) => {
+        console.log("Response " + value);
+        if (value.reCode == 200 && value.reMsg == "操作成功") {
+          return value.data;
+        } else {
+          return value.reMsg;
+        }
+      }).catch((error) => {
+        console.log(error);
+        this.handleError(error);
+      });
+    }
+
+    // 8.  维修麻将机推送类型
+    //     地址:	api/repare/orderRemind
+    //     参数:	token, addr1, addr2, addr3,
+    //       type1, type2, type3, type4, type5, type6 
+    //     返回:	
+    repareOrderRemind(addr1: string, addr2: string, addr3: string, addr4: string, type: string): Promise<any> {
+      let url = "api/repare/orderRemind";
+      let heads = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'};
+      let params = 'addr1=' + addr1 + '&addr2=' + addr2 + '&addr3=' + addr3 + '&addr4=' + addr4 + '&type=' + type;
+
+      return this.httpProvider.httpPostWithAuth(url, heads, params).then((value) => {
+        console.log("Response " + value);
+        if (value.reCode == 200 && value.reMsg == "操作成功") {
+          return value.data;
+        } else {
+          return value.reMsg;
+        }
+      }).catch((error) => {
+        console.log(error);
+        this.handleError(error);
+      });
+    }
+
+    // 9.  维修麻将机订单创建/修改
+    //     地址:	api/repare/orderEdit
+    //     参数:	token, orderNo(新建的时候空, 修改的时候不为空), 
+    //       orderType(1维修订单, 2购买新机器订单, 3购买旧机器订单, 4租麻将机订单, 5安装麻将机或配件订单)
+    //       addr1, addr2, addr3, addr4, addr5, phone, publisher
+    //     返回:
+    repareOrderEdit(orderNo: string, orderType: string, addr1: string, addr2: string,
+       addr3: string, addr4: string, phone: string, publisher: string): Promise<any> {
+      let url = "api/repare/orderEdit";
+      let heads = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'};
+      let params = 'orderNo=' + orderNo;
+      params = params + '&addr1=' + addr1 + '&addr2=' + addr2 + '&addr3=' + addr3 + '&addr4=' + addr4;
+      params = params + '&phone=' + phone + '&publisher' + publisher;
+
+      return this.httpProvider.httpPostWithAuth(url, heads, params).then((value) => {
+        console.log("Response " + value);
+        if (value.reCode == 200 && value.reMsg == "操作成功") {
+          return value.data;
+        } else {
+          return value.reMsg;
+        }
+      }).catch((error) => {
+        console.log(error);
+        this.handleError(error);
+      });
+    }
+}
---
+++ src/provider/service/RepareService.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+
+import { GlobalProvider } from '../GlobalProvider';
+
+import { RepareHttpProvider } from '../http/RepareHttpProvider';
+
+import { HttpException } from "../../exception/HttpException";
+
+/**
+ * The class used to provide service api for user.
+ *
+ */
+@Injectable()
+export class RepareService {
+
+  constructor(public globalProvider: GlobalProvider, public repareHttpProvider: RepareHttpProvider) {
+  }
+
+  repareOraders(addr1: string, addr2: string, addr3: string, addr4: string): Promise<any> {
+    return this.repareHttpProvider.repareOraders(addr1, addr2, addr3, addr4).then((value) => {
+      console.log("userService:---->"+value.status);
+      if (value) {
+        return value;
+      }
+    }).catch((error) => {
+      console.log(error);
+      this.handleError(error);
+    });
+  }
+
+  private handleError(error: Response) {
+    console.error("http error - " + error);
+    throw new HttpException(error.status, "Http Request Error");
+  }
+
+  repareMyOrders(type: string): Promise<any> {
+    return this.repareHttpProvider.repareMyOrders(type).then((value) => {
+      if (value) {
+        return value;
+      }
+    }).catch((error) => {
+      console.log(error);
+      this.handleError(error);
+    });
+  }
+
+  repareOrderUpdate(orderNo: string, type: string): Promise<any> {
+    return this.repareHttpProvider.repareOrderUpdate(orderNo, type).then((value) => {
+      if (value) {
+        return value;
+      }
+    }).catch((error) => {
+      console.log(error);
+      this.handleError(error);
+    });
+  }
+
+  repareOrderRemind(addr1: string, addr2: string, addr3: string, addr4: string, type: string): Promise<any> {
+    return this.repareHttpProvider.repareOrderRemind(addr1, addr2, addr3, addr4, type).then((value) => {
+      if (value) {
+        return value;
+      }
+    }).catch((error) => {
+      console.log(error);
+      this.handleError(error);
+    });
+  }
+
+  repareOrderEdit(orderNo: string, orderType: string, addr1: string, addr2: string,
+    addr3: string, addr4: string, phone: string, publisher: string): Promise<any> {
+      return this.repareHttpProvider.repareOrderEdit(orderNo, orderType, addr1, addr2, addr3, addr4, phone, publisher).then((value) => {
+        if (value) {
+          return value;
+        }
+      }).catch((error) => {
+        console.log(error);
+        this.handleError(error);
+      });
+  }
+}
